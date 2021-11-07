8a7969341a7df1935cc253f7055f515792ed1b6a
hrcg <perrandor@gmail.com>
2021-11-07 22:45:17 +0100

    minor changes

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ mobileBtn.addEventListener("click", () => {
 mobileBtnExit.addEventListener("click", () => {
   nav.classList.remove("menu-btn");
 });
+
+/* Fade in content on scroll */
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var fadeins = $(".fadein");
+
+  for (var i = 0; i < fadeins.length; i++) {
+    var fadein = fadeins[i];
+
+    if ($(fadein).position().top < pageBottom) {
+      $(fadein).addClass("visible");
+    } else {
+      $(fadein).removeClass("visible");
+    }
+  }
+});

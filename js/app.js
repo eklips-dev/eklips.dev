3fe8014a1c2756eebb9449d94808fc183dec1b79
hrcg <perrandor@gmail.com>
2021-11-11 12:14:00 +0100

    changed nav

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-const mobileBtn = document.getElementById("mobile-cta");
-nav = document.querySelector("nav");
-mobileBtnExit = document.getElementById("mobile-exit");
-
-mobileBtn.addEventListener("click", () => {
-  nav.classList.add("menu-btn");
-});
-
-mobileBtnExit.addEventListener("click", () => {
-  nav.classList.remove("menu-btn");
-});
-
 /* Fade in content on scroll */
 $(document).on("scroll", function () {
   var pageTop = $(document).scrollTop();
@@ -47,4 +35,21 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }

b75c7bf158baa81aafc4ec776720ed0f59a8da58
hrcg <perrandor@gmail.com>
2021-11-21 22:43:37 +0100

    seperated js file

--- js/app.js
+++ js/app.js
@@ -147,12 +147,3 @@ $(window).scroll(function () {
     $(".navbar").removeClass("bg-scrolling");
   }
 });
-
-// Play video in about page
-var interval = setInterval(function () {
-  var countForVideo = document.getElementById("vid").readyState;
-  if (countForVideo == 4) {
-    document.getElementById("vid").play();
-    clearInterval(interval);
-  }
-}, 2000);

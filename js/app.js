b59ef65e6195f43dacc219ec90a02fcda107a948
hrcg <perrandor@gmail.com>
2021-11-21 20:15:00 +0100

    fixed folder names

--- js/app.js
+++ js/app.js
@@ -147,3 +147,12 @@ $(window).scroll(function () {
     $(".navbar").removeClass("bg-scrolling");
   }
 });
+
+// Play video in about page
+var interval = setInterval(function () {
+  var countForVideo = document.getElementById("vid").readyState;
+  if (countForVideo == 4) {
+    document.getElementById("vid").play();
+    clearInterval(interval);
+  }
+}, 2000);

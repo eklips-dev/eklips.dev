ff36bbf0cb773776a9184802121e73fee19f4f58
hrcg <perrandor@gmail.com>
2021-11-26 16:17:20 +0100

    minifed js

--- js/videoplay.js
+++ js/videoplay.js
@@ -1,8 +1,1 @@
-// Play video in about page
-var interval = setInterval(function () {
-  var countForVideo = document.getElementById("vid").readyState;
-  if (countForVideo == 4) {
-    document.getElementById("vid").play();
-    clearInterval(interval);
-  }
-}, 2000);
+var interval=setInterval(function(){4==document.getElementById("vid").readyState&&(document.getElementById("vid").play(),clearInterval(interval))},2e3);

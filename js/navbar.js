172e2a19516eceb932dd5e3d6896abe8e557eaa5
hrcg <erald@kuci.dev>
2022-07-17 14:03:29 +0200

    repo cleanup

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("logo").style.maxWidth = "35%";
-  } else {
-    document.getElementById("logo").style.maxWidth = "50%";
-  }
-}
